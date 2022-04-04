0759abf85e5f2b52db6f4deaa7bacf93237ec0a2
Apoorv khandelwal <apoorvkhandelwal86@gmail.com>
2022-04-04 17:37:04 +0530

    Mongo DB set up

--- EJS.js
+++ EJS.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Blog = require('./model/blogs');
 
 const app = express();
 
@@ -13,7 +14,29 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 app.disable('etag');
 
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+    blog.save().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+});
 
+app.get('/all-blogs', (req, res) => {
+    Blog.find().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
+// routes
 app.get('/', (req, res) => {
     res.render('index');
 });
